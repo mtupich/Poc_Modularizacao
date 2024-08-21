05b10d72c857cd25d19554cc3f3fa708c3de8112
Maria <mariatupich@gmail.com>
2024-08-21 20:20:43 -0300

    first commit

---
+++ Poc_Modularizacao/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Poc_Modularizacao
+//
+//  Created by Maria Tupich on 21/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
